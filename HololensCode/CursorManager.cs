b5caf4f012f8c45e3744d8748dfb6fc096a46146
scanavan <scanavan@binghamton.edu>
2016-10-12 08:54:40 -0400

    Added modified gaze and cursor manager code for hololens

---
+++ HololensCode/CursorManager.cs
@@ -0,0 +1,53 @@
+﻿using HoloToolkit;
+using UnityEngine;
+
+/// <summary>
+/// CursorManager class takes Cursor GameObjects.
+/// One that is on Holograms and another off Holograms.
+/// Shows the appropriate Cursor when a Hologram is hit.
+/// Places the appropriate Cursor at the hit position.
+/// Matches the Cursor normal to the hit surface.
+/// </summary>
+public class CursorManager : Singleton<CursorManager>
+{
+    [Tooltip("Drag the Cursor object to show when it hits a hologram.")]
+    public GameObject CursorOnHolograms;
+
+    [Tooltip("Drag the Cursor object to show when it does not hit a hologram.")]
+    public GameObject CursorOffHolograms;
+
+    void Awake()
+    {
+        if (CursorOnHolograms == null || CursorOffHolograms == null)
+        {
+            return;
+        }
+
+        // Hide the Cursors to begin with.
+        CursorOnHolograms.SetActive(false);
+        CursorOffHolograms.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (GazeManager.Instance == null || CursorOnHolograms == null || CursorOffHolograms == null)
+        {
+            return;
+        }
+
+        if(GazeManager.Instance.Position.y > 0)
+        {
+            CursorOffHolograms.SetActive(false);
+            CursorOnHolograms.SetActive(true);
+        }
+        else
+        {
+            CursorOffHolograms.SetActive(true);
+            CursorOnHolograms.SetActive(false);
+        }
+        // 2.b: Assign gameObject's transform position equals GazeManager's instance Position.
+        gameObject.transform.position = GazeManager.Instance.Position;
+        // 2.b: Assign gameObject's transform up vector equals GazeManager's instance Normal.
+        gameObject.transform.up = GazeManager.Instance.Normal;
+    }
+}
---
+++ HololensCode/GazeManager.cs
@@ -0,0 +1,62 @@
+﻿using HoloToolkit;
+using UnityEngine;
+
+/// <summary>
+/// GazeManager determines the location of the user's gaze, hit position and normals.
+/// </summary>
+public class GazeManager : Singleton<GazeManager>
+{
+    [Tooltip("Maximum gaze distance for calculating a hit.")]
+    public float MaxGazeDistance = 5.0f;
+
+    [Tooltip("Select the layers raycast should target.")]
+    public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
+
+    /// <summary>
+    /// Physics.Raycast result is true if it hits a Hologram.
+    /// </summary>
+    public bool Hit { get; private set; }
+
+    /// <summary>
+    /// HitInfo property gives access
+    /// to RaycastHit public members.
+    /// </summary>
+    public RaycastHit HitInfo { get; private set; }
+
+    /// <summary>
+    /// Position of the user's gaze.
+    /// </summary>
+    public Vector3 Position { get; private set; }
+
+    /// <summary>
+    /// RaycastHit Normal direction.
+    /// </summary>
+    public Vector3 Normal { get; private set; }
+
+    private GazeStabilizer gazeStabilizer;
+    private Vector3 gazeOrigin;
+    private Vector3 gazeDirection;
+
+    void Awake()
+    {
+        /* TODO: DEVELOPER CODING EXERCISE 3.a */
+
+        // 3.a: GetComponent GazeStabilizer and assign it to gazeStabilizer.
+        gazeStabilizer = GetComponent<GazeStabilizer>();
+    }
+
+    private void Update()
+    {
+        // 2.a: Assign Camera's main transform position to gazeOrigin.
+        gazeOrigin = Camera.main.transform.position;
+
+        // 2.a: Assign Camera's main transform forward to gazeDirection.
+        gazeDirection = Camera.main.transform.forward;
+
+        // 2.a: Assign Position to be gazeOrigin plus MaxGazeDistance times gazeDirection.
+        Position = gazeOrigin + (gazeDirection * MaxGazeDistance);
+
+        // 2.a: Assign Normal to be the user's gazeDirection.
+        Normal = gazeDirection;
+    }
+}
